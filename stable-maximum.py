62ed1ec5adef3b2de905d406d7a9dba3c0adb2bb
MaximeDaigle <maximedaigle95@gmail.com>
2017-12-13 15:48:32 -0500

    Update stable-maximum.py

--- stable-maximum.py
+++ stable-maximum.py
@@ -2,14 +2,6 @@ import itertools
 
 #Find the maximum independent set in a graph with a greedy algorithm
 
-# "In graph theory, an independent set or stable set is a set of vertices in a graph, no two of which are adjacent.
-# That is, it is a set S of vertices such that for every two vertices in S, there is no edge connecting the two.
-# Equivalently, each edge in the graph has at most one endpoint in S.
-# The size of an independent set is the number of vertices it contains.
-# Independent sets have also been called internally stable sets.
-# A maximum independent set is an independent set of largest possible size for a given graph G.
-# The problem of finding such a set is called the maximum independent set problem and is an NP-hard optimization problem."
-
 graph_test = [[0,1,0,0,0,1],
          [1,0,1,0,0,1],
          [0,1,0,1,0,0],
